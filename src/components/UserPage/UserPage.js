571ed37718a3097c2b6478008d1bd0fca0f7be6d
Sasha Milenkovic <sashamilenkovic2@gmail.com>
2018-08-23 13:43:40 -0500

    styling userpage

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
 
+import './UserPage.css';
+
 
 
 const mapStateToProps = state => ({
@@ -23,8 +25,18 @@ class UserPage extends Component {
 render() {
     return (
       <div>
-        <Link to="/game"></Link>
-        <img src="/images/Icons/logout.png" />
+        <div>
+          <h1 className="header">POKERBOT</h1>
+        </div>
+        <div className="grid-3">
+          <Link to="/game"><img className="images" src="/images/Icons/startGame.png" /></Link>
+          <Link to="/statistics"><img className="images" src="/images/Icons/statistics.png" /></Link>
+          <Link to="/settings"><img className ="images" src="/images/Icons/settings.png" /></Link>
+          <Link to="/preferences"><img className="images" src="/images/Icons/preferences.png" /></Link>
+          <Link to="/account"><img className="images" src="/images/Icons/account.png" /></Link>
+          <Link to="/game"><img className="images" src="/images/Icons/logout.png" /></Link>
+        </div>
+
       </div>
 
 
